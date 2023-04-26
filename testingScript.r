dcc75c8f45805f1de0c0913369dbf7c5fb1d8e92
cantlap <cantlap@ccf.org>
2023-04-26 10:09:34 -0400

    testEditting

--- testingScript.r
+++ testingScript.r
@@ -1,3 +1,33 @@
 1+1
 library(devtools)
 #!testing file scripting
+
+library(SNPRelate)
+library(SeqArray)
+
+
+start_file_name <- "split.chr"
+end_file_name <- ".gds"
+
+
+for (chr in 1:22){
+    
+    full_file_name <- paste(start_file_name, chr, end_file_name, sep = "")
+    fileName <- seqOpen(full_file_name)
+
+    # open an example dataset (HapMap)
+    genofile <- fileName
+
+
+    
+    snpset <- snpgdsSelectSNP(genofile, missing.rate=0.95)
+    out_file_name <- paste("test", chr, "file", sep = "_")
+    setwd("..")
+    setwd(paste0(getwd(), "/grm_files"))
+    seqGDS2BED(genofile, out.fn = out_file_name, multi.row=FALSE, verbose=TRUE)
+    setwd("..")
+    setwd(paste0(getwd(), "/input_agds_files"))
+
+    # close the genotype file
+    seqClose(genofile)
+}
